7ce15f5bbdfed296c6a74b85408dd9723a0e9d2e
Anita Wangui <anitawangui22@gmail.com>
2024-12-02 17:30:44 +0300

    add properties and attribute

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count, brand="Unknown", size="Unknown"):
+        self.title = title
+        self._page_count = page_count
+        self._brand=brand
+        self._size=size
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def brand(self):
+        return self._brand
     
+    @brand.setter   
+    def brand(self, value):
+        self._brand = value
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        self._size = value
+        
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,58 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color="black", price="unknown", cobble="false"):
+        self._brand = brand
+        self._size = size
+        self._color = color
+        self._price = price
+        self._cobble = cobble
+        self._condition = "New"  
+
+    def cobble(self):
+        self._cobble = "true"
+        self._condition = "New"  
+        print("Your shoe is as good as new!")
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        self._color = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        self._condition = value
